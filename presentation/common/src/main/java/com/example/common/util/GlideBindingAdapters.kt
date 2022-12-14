93581f5d1b4dba3a5d6b2984e38a8306852b5d23
duru.hi <hwanggyuil@gmail.com>
2022-12-14 19:48:16 +0900

    [Prototype] Common > Glide, Recycler 유틸 추가

---
+++ presentation/common/src/main/java/com/example/common/util/GlideBindingAdapters.kt
@@ -0,0 +1,40 @@
+package com.example.common.util
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+@BindingAdapter("glide1000")
+fun ImageView.bindGlide1000(uri: String) {
+    Glide.with(context)
+        .load(uri)
+        .transform(CenterCrop(), RoundedCorners(1000))
+        .into(this)
+}
+
+@BindingAdapter("glide300")
+fun ImageView.bindGlide300(uri: String) {
+    Glide.with(context)
+        .load(uri)
+        .transform(CenterCrop(), RoundedCorners(300))
+        .into(this)
+}
+
+@BindingAdapter("glide100")
+fun ImageView.bindGlide100(uri: String) {
+    Glide.with(context)
+        .load(uri)
+        .transform(CenterCrop(), RoundedCorners(100))
+        .into(this)
+}
+
+@BindingAdapter("glide")
+fun ImageView.bindGlide(uri: String) {
+    Glide.with(context)
+        .load(uri)
+        .transform(CenterCrop(), RoundedCorners(300))
+        .into(this)
+}
+
